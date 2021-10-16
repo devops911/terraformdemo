7f2aff820323207c0eb2401aef3e6fe3ff6cdcae
devops911 <86989276+devops911@users.noreply.github.com>
2021-10-16 17:42:10 -0500

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -1,10 +1,3 @@
-provider "azurerm" {
-  subscription_id = "8690ba9a-5388-4b31-90df-15acb9170e0e"
-  client_id       = "f0abb33a-9eb9-4199-ba7f-c7183350492c"
-  client_secret   = "D_GtMa7uwt3q0ZUB8WOWwGwo~oPwXqLFyQ"
-  tenant_id       = "97bc43fb-b749-4757-955f-8167fc70e670"
-}
-
 # Azure Provider source and version being used
 terraform {
   required_providers {
@@ -13,6 +6,13 @@ terraform {
       version = "=2.46.0"
     }
   }
+}
+
+provider "azurerm" {
+  subscription_id = "8690ba9a-5388-4b31-90df-15acb9170e0e"
+  client_id       = "f0abb33a-9eb9-4199-ba7f-c7183350492c"
+  client_secret   = "D_GtMa7uwt3q0ZUB8WOWwGwo~oPwXqLFyQ"
+  tenant_id       = "97bc43fb-b749-4757-955f-8167fc70e670"
 }
  
 
