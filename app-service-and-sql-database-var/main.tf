0cd544fc92e4708925241c2a5e507ec4b3ffa49f
devops911 <86989276+devops911@users.noreply.github.com>
2021-10-16 17:38:00 -0500

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -15,10 +15,7 @@ terraform {
   }
 }
  
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
+
 
 resource "azurerm_resource_group" "RG-Terraform" {
   name     = "terraform-resource-group"
