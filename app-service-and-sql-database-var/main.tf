78ebbc92f77797505d779e4c72418ab1758c7844
devops911 <86989276+devops911@users.noreply.github.com>
2021-10-17 18:47:08 -0500

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -9,10 +9,10 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "8690ba9a-5388-4b31-90df-15acb9170e0e"
-  client_id       = "f0abb33a-9eb9-4199-ba7f-c7183350492c"
-  client_secret   = "D_GtMa7uwt3q0ZUB8WOWwGwo~oPwXqLFyQ"
-  tenant_id       = "97bc43fb-b749-4757-955f-8167fc70e670"
+  subscription_id = "8690ba9a-5388-4b31-90df"
+  client_id       = "f0abb33a-9eb9-4199-ba7f"
+  client_secret   = "D_GtMa7uwt3q0ZUB8WOWwGwo"
+  tenant_id       = "97bc43fb-b749-4757-955f"
   features {}
 }
  
